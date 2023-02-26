7bfdaa74c13f064fcc07c87eb72451d3ee883509
jupbc <jupbcan100@hotmail.com>
2023-02-26 18:08:47 -0500

    Funcionalidades del sprint 2

--- app/src/main/java/co/edu/unipiloto/whitesound/EditarPartituraActivity.java
+++ app/src/main/java/co/edu/unipiloto/whitesound/EditarPartituraActivity.java
@@ -1,10 +1,19 @@
 package co.edu.unipiloto.whitesound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,10 +21,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditarPartituraActivity extends AppCompatActivity {
 
     public static final String ARCHIVO = "partitura.wsnd";
+    private BottomNavigationView aep_menu_inferior;
+    List<String> figurasMusicales;
+    List<String> silenciosMusicales;
+    Dialog dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +54,97 @@ public class EditarPartituraActivity extends AppCompatActivity {
         //Inicializar vistas
         TextView aep_tv_titulo = (TextView) findViewById(R.id.aep_tv_titulo);
         TextView aep_tv_autor = (TextView) findViewById(R.id.aep_tv_autor);
+        aep_menu_inferior = (BottomNavigationView) findViewById(R.id.aep_menu_inferior);
+        dialogo = new Dialog(this);
+        dialogo.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         //Inicializar textos de partitura
         aep_tv_titulo.setText(tituloPartitura);
         aep_tv_autor.setText(nombreAutor);
+
+        //Menu inferior
+        aep_menu_inferior.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+
+                    case R.id.miep_notas:
+                        abrirDialogoFiguraMusical();
+                        break;
+                    case R.id.miep_silencios:
+                        abrirDialogoSilencioMusical();
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private List<String> getFiguras() {
+
+        figurasMusicales = new ArrayList<String>();
+
+        figurasMusicales.add("Redonda");
+        figurasMusicales.add("Blanca");
+        figurasMusicales.add("Negra");
+        figurasMusicales.add("Corchea");
+        figurasMusicales.add("Semicorchea");
+        figurasMusicales.add("Fusa");
+        figurasMusicales.add("Semifusa");
+        figurasMusicales.add("Garrapatea");
+
+        return figurasMusicales;
+    }
+
+    private List<String> getSilencios() {
+
+        silenciosMusicales = new ArrayList<String>();
+
+        silenciosMusicales.add("Silencio de redonda");
+        silenciosMusicales.add("Silencio de blanca");
+        silenciosMusicales.add("Silencio de negra");
+        silenciosMusicales.add("Silencio de corchea");
+        silenciosMusicales.add("Silencio de semicorchea");
+        silenciosMusicales.add("Silencio de fusa");
+        silenciosMusicales.add("Silencio de semifusa");
+        silenciosMusicales.add("Silencio de garrapatea");
+
+        return silenciosMusicales;
+    }
+
+    //**** DIÁLOGOS ****
+
+    //Diálogo para elegir una figura musical
+    private void abrirDialogoFiguraMusical() {
+
+        //Mostrar diálogo
+        dialogo.setContentView(R.layout.popup_figura_musical);
+        dialogo.show();
+
+        //Inicializar vistas del diálogo
+        ListView pfm_lv_figura_musical = (ListView) dialogo.findViewById(R.id.pfm_lv_figura_musical);
+        AdaptadorListaFigura adaptador = new AdaptadorListaFigura(this, getFiguras());
+        pfm_lv_figura_musical.setAdapter(adaptador);
+    }
+
+    //Diálogo para elegir un silencio musical
+    private void abrirDialogoSilencioMusical() {
+
+        //Mostrar diálogo
+        dialogo.setContentView(R.layout.popup_silencio_musical);
+        dialogo.show();
+
+        //Inicializar vistas del diálogo
+        ListView psm_lv_figura_musical = (ListView) dialogo.findViewById(R.id.psm_lv_silencio_musical);
+        AdaptadorListaSilencio adaptador = new AdaptadorListaSilencio(this, getSilencios());
+        psm_lv_figura_musical.setAdapter(adaptador);
+    }
+
+    //Cerrar cualquier diálogo
+    private void cerrarDialogo() {
+        dialogo.dismiss();
     }
 
     public String contenidoArchivoPartitura(String archivo) throws FileNotFoundException {
--- app/src/main/java/co/edu/unipiloto/whitesound/GestionarPartituraActivity.java
+++ app/src/main/java/co/edu/unipiloto/whitesound/GestionarPartituraActivity.java
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class GestionarPartituraActivity extends AppCompatActivity {
 
@@ -27,7 +26,7 @@ public class GestionarPartituraActivity extends AppCompatActivity {
     TextView agp_tv_partituras_anteriores;
     ListView agp_lv_partituras;
     List<String> partituras;
-    Dialog dialog;
+    Dialog dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,53 +37,43 @@ public class GestionarPartituraActivity extends AppCompatActivity {
         agp_btn_crear_partitura = (Button) findViewById(R.id.agp_btn_crear_partitura);
         agp_tv_partituras_anteriores = (TextView) findViewById(R.id.agp_tv_partituras_anteriores);
         agp_lv_partituras = (ListView) findViewById(R.id.agp_lv_partituras);
-        dialog = new Dialog(this);
+        dialogo = new Dialog(this);
+        dialogo.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
-        //Preparar lista de partituras
-        AdaptadorListaPartitura adaptador = new AdaptadorListaPartitura(this, getPartituras());
-        agp_lv_partituras.setAdapter(adaptador);
-
-        //Texto de partituras anteriores
-        if (!partituras.isEmpty()) {
-            agp_tv_partituras_anteriores.setVisibility(View.VISIBLE);
-        }
+        //Cargar la lista de partituras almacenadas en el dispositivo
+        recargarListaPartituras();
 
         //Item click listener de la lista de partituras
         agp_lv_partituras.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                abrirOpcionesPartitura(i);
+                abrirDialogoOpcionesPartitura(i);
             }
         });
 
-        //Click listener del botón para crear una nueva partitura
+        //Click listener del botón que abre el diálogo para crear una nueva partitura
         agp_btn_crear_partitura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirDialogo();
+                abrirDialogoNuevaPartitura();
             }
         });
     }
 
-    private void abrirOpcionesPartitura(int i) {
+    //**** DIÁLOGOS ****
 
-        String partitura = partituras.get(i);
-        Intent intent = new Intent(this, EditarPartituraActivity.class);
-        intent.putExtra(EditarPartituraActivity.ARCHIVO,partitura);
-        startActivity(intent);
-    }
+    //Diálogo para crear una nueva partitura
+    private void abrirDialogoNuevaPartitura() {
 
-    //Abrir dialogo de datos de la partitura
-    private void abrirDialogo() {
+        //Mostrar diálogo
+        dialogo.setContentView(R.layout.popup_datos_partitura);
+        dialogo.show();
 
-        //Mostrar dialogo
-        dialog.setContentView(R.layout.popup_datos_partitura);
-        dialog.show();
+        //Inicializar vistas del diálogo
+        Button pdp_btn_cancelar = (Button) dialogo.findViewById(R.id.pdp_btn_cancelar);
+        Button pdp_btn_aceptar = (Button) dialogo.findViewById(R.id.pdp_btn_aceptar);
 
-        //Inicializar vistas del dialogo
-        Button pdp_btn_cancelar = (Button) dialog.findViewById(R.id.pdp_btn_cancelar);
-        Button pdp_btn_aceptar = (Button) dialog.findViewById(R.id.pdp_btn_aceptar);
-
+        //Click listener del botón para cancelar la creación de una nueva partitura
         pdp_btn_cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,6 +81,7 @@ public class GestionarPartituraActivity extends AppCompatActivity {
             }
         });
 
+        //Click listener del botón para crear una nueva partitura
         pdp_btn_aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -100,50 +90,116 @@ public class GestionarPartituraActivity extends AppCompatActivity {
         });
     }
 
+    //Diálogo para seleccionar la acción a tomar con una partitura
+    private void abrirDialogoOpcionesPartitura(int i) {
+
+        //Mostrar diálogo
+        dialogo.setContentView(R.layout.popup_opciones_partitura);
+        dialogo.show();
+
+        //Inicializar vistas del diálogo
+        ImageButton pop_imgbtn_eliminar_partitura = (ImageButton) dialogo.findViewById(R.id.pop_imgbtn_eliminar_partitura);
+        ImageButton pop_imgbtn_editar_partitura = (ImageButton) dialogo.findViewById(R.id.pop_imgbtn_editar_partitura);
+        Button pop_btn_cancelar = (Button) dialogo.findViewById(R.id.pop_btn_cancelar);
+
+        //Click listener del botón para eliminar una partitura
+        pop_imgbtn_eliminar_partitura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eliminarPartitura(i);
+            }
+        });
+
+        //Click listener del botón para editar una partitura
+        pop_imgbtn_editar_partitura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editarPartitura(i);
+            }
+        });
+
+        //Click listener del botón para cancelar la edición o eliminación de una nueva partitura
+        pop_btn_cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cerrarDialogo();
+            }
+        });
+    }
+
+    //Cerrar cualquier diálogo
+    private void cerrarDialogo() {
+        dialogo.dismiss();
+    }
+
+    //**** CRUD PARTITURAS ****
+
+    //Crear una nueva partitura
     private void crearNuevaPartitura() {
 
         //Inicializar vistas del dialogo
-        TextView pdp_et_nombre_autor = (EditText) dialog.findViewById(R.id.pdp_et_nombre_autor);
-        TextView pdp_et_nombre_partitura = (EditText) dialog.findViewById(R.id.pdp_et_nombre_partitura);
+        TextView pdp_et_nombre_autor = (EditText) dialogo.findViewById(R.id.pdp_et_nombre_autor);
+        TextView pdp_et_nombre_partitura = (EditText) dialogo.findViewById(R.id.pdp_et_nombre_partitura);
 
-        //Obtener texto de los editText
+        //Obtener texto de los edit text del diálogo
         String nombreAutor = pdp_et_nombre_autor.getText().toString();
         String nombrePartitura = pdp_et_nombre_partitura.getText().toString();
 
+        //Caso en el que no se pone un autor
         if(nombreAutor.length()==0){
             nombreAutor = "Sin autor";
         }
 
+        //Caso en que no se pone título a la partitura
         if(nombrePartitura.length()==0){
             nombrePartitura = "Sin título";
         }
 
-        String nombreArchivo = guardarPartitura(nombrePartitura, nombreAutor);
+        //Guardar archivo de la nueva partitura
+        guardarPartitura(nombrePartitura, nombreAutor);
+
+        //Abrir editor de la última partitura creada
+        editarPartitura(-1);
+    }
+
+    //Editar una partitura
+    private void editarPartitura(int i) {
+
+        //Inicializar variable que contiene el nombre del archivo de la partitura
+        String nombreArchivo = "";
+
+        //Asignar nombre del archvio de partitura que se va a editar
+        if(i>=0){
+            nombreArchivo = partituras.get(i);
+        } else {
+            nombreArchivo = partituras.get(partituras.size()-1);
+        }
+
+        //Inicializar el intent de la actividad editor de partitura
+        Intent intent = new Intent(this, EditarPartituraActivity.class);
+        intent.putExtra(EditarPartituraActivity.ARCHIVO,nombreArchivo);
+
+        //Cerrar el diálogo para crear una nueva partitura
+        cerrarDialogo();
 
         //Iniciar actividad editor de partitura
-        Intent intent = new Intent(this, EditarPartituraActivity.class);
-
-        intent.putExtra(EditarPartituraActivity.ARCHIVO, nombreArchivo);
-
-        cerrarDialogo();
         startActivity(intent);
     }
 
-    private void cerrarDialogo() {
-        dialog.dismiss();
-    }
-
-    private String guardarPartitura(String tituloPartitura, String autor){
+    //Guardar una partitura
+    private void guardarPartitura(String tituloPartitura, String autor){
 
         //Reemplazar a minusculas y quitar espacios en blanco en el título de la partitura.
         //Asignar nombre del archivo
-        String nombreArchivo = tituloPartitura.toLowerCase().replaceAll("\\s","_") + ".wsnd";
+        String nombreArchivo = tituloPartitura.trim().toLowerCase().replaceAll("\\s","_") + ".wsnd";
 
-        String contenido =   tituloPartitura.replaceAll("\\s","_") + " "
+        String contenido = tituloPartitura.replaceAll("\\s","_") + " "
                 + autor.replaceAll("\\s", "_");
 
+        //Inicializar output stream
         FileOutputStream outputStream;
 
+        //Escribir contenido de la partitura en el archivo
         try {
             outputStream = openFileOutput(nombreArchivo, Context.MODE_PRIVATE);
             outputStream.write(contenido.getBytes());
@@ -153,16 +209,39 @@ public class GestionarPartituraActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        return nombreArchivo;
+        //Recargar la lista de partituras
+        recargarListaPartituras();
     }
+
+    //Eliminar una partitura
+    private void eliminarPartitura(int i){
+
+        //Inicializar nombre del archivo de partitura a eliminar
+        String nombreArchivo = partituras.get(i);
+
+        //Obtener archivo que se desea eliminar
+        File archivoPartitura = new File(getFilesDir(), nombreArchivo);
+
+        //Eliminar archivo
+        archivoPartitura.delete();
+
+        //Recargar lista de partituras
+        recargarListaPartituras();
+
+        //Cerrar diálogo de opciones de partitura
+        cerrarDialogo();
+    }
+
+    //**** OTRAS FUNCIONES ****
 
     //Obtener lista de partituras
     private List<String> getPartituras() {
 
-        //Recuperar partituras
+        //Recuperar todos los archivos de partituras que se han guardado
         File[] archivos = getFilesDir().listFiles();
         partituras = new ArrayList<>();
 
+        //Añadir el nombre de los archivos a la lista partituras
         for(File archivo : archivos ){
             partituras.add(archivo.getName());
         }
@@ -175,7 +254,19 @@ public class GestionarPartituraActivity extends AppCompatActivity {
     public void onResume() {
 
         super.onResume();
+
+        recargarListaPartituras();
+    }
+
+    public void recargarListaPartituras(){
+
         AdaptadorListaPartitura adaptador = new AdaptadorListaPartitura(this, getPartituras());
         agp_lv_partituras.setAdapter(adaptador);
+
+        if (!partituras.isEmpty()) {
+            agp_tv_partituras_anteriores.setVisibility(View.VISIBLE);
+        }else{
+            agp_tv_partituras_anteriores.setVisibility(View.INVISIBLE);
+        }
     }
 }
